eab49b03bd33e74d3d97ad8f7ed9656c05592847
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-04-07 23:19:34 +0300

    New project files

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,42 @@
+-import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+    @Test
+    void successfulGoogleSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+    @Test
+    void successfulYaSearchTest() {
+        open("https://www.ya.ru/");
+        $("#text").setValue("selenide").pressEnter();
+        $("#search-result").shouldHave(text("selenide.org"));
+    }
+
+    @Test
+    void successfulYahooSearchTest(){
+        open("https://search.yahoo.com/");
+        $("#yschsp").setValue("selenide").pressEnter();
+        $("div#results").shouldHave(text("selenide.org"));
+    }
+
+    @Test
+    void successfulBingSearchTest(){
+        open("https://www.bing.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=b_results]").shouldHave(text("https://selenide.org"));
+    }
+
+    @Test
+    void successfulDuckduckgoSearchTest() {
+        open("https://duckduckgo.com/");
+        $("#searchbox_input").setValue("selenide").pressEnter();
+        $(".react-results--main").shouldHave(text("https://selenide.org"));
+    }
+}

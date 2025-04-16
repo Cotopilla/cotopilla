92dffb80a4c2123113e57da24d84ba14864d6d84
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-04-17 00:13:48 +0300

    add actionTest

--- src/test/java/github/GithubSelenideTest.java
+++ src/test/java/github/GithubSelenideTest.java
@@ -1,12 +1,8 @@
 package github;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.DragAndDropOptions;
-import com.codeborne.selenide.HoverOptions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
-import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.DragAndDropOptions.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -85,12 +81,11 @@ public class GithubSelenideTest {
     }
 
     @Test
-    void dragAndDropTest() {
+    void actionTest() {
         //Запрограммируйте Drag&Drop с помощью Selenide.actions()
         // - Откройте https://the-internet.herokuapp.com/drag_and_drop
         // - Перенесите прямоугольник А на место В
         // - Проверьте, что прямоугольники действительно поменялись
-        // - В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест, если использовать её вместо actions()
 
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a header").shouldHave(text("A"));
@@ -100,7 +95,15 @@ public class GithubSelenideTest {
                 .release()
                 .perform();
         $("#column-a header").shouldHave(text("B"));
-        $("#column-b").dragAndDrop(to("#column-a"));
+    }
+
+    @Test
+    void dragAndDropTest() {
+        // - В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест, если использовать её вместо actions()
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a header").shouldHave(text("A"));
+        $("#column-b").dragAndDrop(to("#column-a"));
+        $("#column-a header").shouldHave(text("B"));
     }
 }

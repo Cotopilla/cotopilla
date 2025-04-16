34b0a8e184981755ba31f25eb727d2654f6360ca
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-04-17 00:06:19 +0300

    add dragAndDropTest

--- src/test/java/github/GithubSelenideTest.java
+++ src/test/java/github/GithubSelenideTest.java
@@ -1,16 +1,20 @@
 package github;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.DragAndDropOptions;
 import com.codeborne.selenide.HoverOptions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.DragAndDropOptions.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GithubSelenideTest {
     @Test
-    void shouldFindSelenideRepositoryAtTheTop() {
+    void shouldFindSelenideRepositoryAtTheTopTest() {
 
         // открыть главную страницу
         open("https://github.com/");
@@ -24,7 +28,7 @@ public class GithubSelenideTest {
     }
 
     @Test
-    void andreiSolntsevShouldBeTheFirstContributor() {
+    void andreiSolntsevShouldBeTheFirstContributorTest() {
         // открыть страницу репозитория селенида
         open("https://github.com/selenide/selenide");
         // подвести мышку к первому аватару из блока contributors
@@ -53,7 +57,7 @@ public class GithubSelenideTest {
                 .sibling(0)
                 .$("pre")
                 .shouldHave(text(
-                                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "@ExtendWith({SoftAssertsExtension.class})\n" +
                                 "        class Tests {\n" +
                                 "            @Test\n" +
                                 "            void test() {\n" +
@@ -71,14 +75,32 @@ public class GithubSelenideTest {
     void hoverTest() {
         //На главной странице GitHub выберите: Меню -> Solutions -> Enterprize
         // (с помощью команды hover для Solutions)
+        // Убедитесь, что загрузилась нужная страница
+        // (например, что заголовок: "The AI-powered developer platform.").
+
         open("https://github.com/");
         $(".HeaderMenu-nav").$(byText("Solutions")).hover();
         $("a[href='https://github.com/enterprise']").click();
-        // Убедитесь, что загрузилась нужная страница
-        // (например, что заголовок: "The AI-powered developer platform.").
         $("#hero-section-brand-heading").shouldHave(text("The AI-powered\ndeveloper platform"));
+    }
 
+    @Test
+    void dragAndDropTest() {
+        //Запрограммируйте Drag&Drop с помощью Selenide.actions()
+        // - Откройте https://the-internet.herokuapp.com/drag_and_drop
+        // - Перенесите прямоугольник А на место В
+        // - Проверьте, что прямоугольники действительно поменялись
+        // - В Selenide есть команда $(element).dragAndDrop($(to-element)), проверьте работает ли тест, если использовать её вместо actions()
 
-
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a header").shouldHave(text("A"));
+        actions().moveToElement($("#column-a"))
+                .clickAndHold()
+                .moveToElement($("#column-b"))
+                .release()
+                .perform();
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b").dragAndDrop(to("#column-a"));
+        $("#column-a header").shouldHave(text("A"));
     }
 }

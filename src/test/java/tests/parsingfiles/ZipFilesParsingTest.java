d138ab7f698a273d5428a2eda5a88f8ba400db62
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-05-18 14:48:17 +0300

    add emptyZip

--- src/test/java/tests/parsingfiles/ZipFilesParsingTest.java
+++ src/test/java/tests/parsingfiles/ZipFilesParsingTest.java
@@ -6,6 +6,7 @@ import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
@@ -14,27 +15,32 @@ import java.util.zip.ZipInputStream;
 
 public class ZipFilesParsingTest {
 
+    private String zipFile = "parsingfiles/readcheckfiles.zip";
+    private String emptyZipFile = "parsingfiles/empty.zip";
     private ClassLoader cl = ZipFilesParsingTest.class.getClassLoader();
 
     @Test
     void pdfFromZipParsingTest() throws Exception {
-        try (InputStream is = cl.getResourceAsStream("parsingfiles/readcheckfiles.zip");
-             ZipInputStream zis = new ZipInputStream(is)) {
-            ZipEntry zipEntry;
-            while ((zipEntry = zis.getNextEntry()) != null) {
-                if (zipEntry.getName().endsWith(".pdf")) {
-                    PDF pdf = new PDF(zis);
-                    Assertions.assertTrue(pdf.text.contains("Тестовый PDF-документ"));
-                }
+        try (InputStream is = cl.getResourceAsStream(emptyZipFile)) {
+            try (ZipInputStream zis = new ZipInputStream(is)) {
+                if (zis.getNextEntry() != null) {
+                    ZipEntry zipEntry;
+                    while ((zipEntry = zis.getNextEntry()) != null) {
+                        if (zipEntry.getName().endsWith(".pdf")) {
+                            PDF pdf = new PDF(zis);
+                            Assertions.assertTrue(pdf.text.contains("Тестовый PDF-документ"));
+                        }
+                    }
+                } else throw new FileNotFoundException("Архив пустой");
             }
         }
     }
 
     @Test
     void xlsFromZipParsingTest() throws Exception {
-        try (InputStream is = cl.getResourceAsStream("parsingfiles/readcheckfiles.zip")) {
-            if (is != null) {
-                try (ZipInputStream zis = new ZipInputStream(is)) {
+        try (InputStream is = cl.getResourceAsStream(zipFile)) {
+            try (ZipInputStream zis = new ZipInputStream(is)) {
+                if (zis.getNextEntry() != null) {
                     ZipEntry zipEntry;
                     while ((zipEntry = zis.getNextEntry()) != null) {
                         if (zipEntry.getName().endsWith(".xlsx")) {
@@ -43,7 +49,7 @@ public class ZipFilesParsingTest {
                             Assertions.assertEquals("Тестовый XLS-документ", actualValue);
                         }
                     }
-                }
+                } else throw new FileNotFoundException("Архив пустой");
             }
         }
     }
@@ -51,24 +57,26 @@ public class ZipFilesParsingTest {
     @Test
     void csvFromZipParsingTest() throws Exception {
         try (ZipInputStream zis = new ZipInputStream(
-                cl.getResourceAsStream("parsingfiles/readcheckfiles.zip")
+                cl.getResourceAsStream(zipFile)
         )) {
-            ZipEntry zipEntry;
-            while ((zipEntry = zis.getNextEntry()) != null) {
-                if (zipEntry.getName().endsWith(".csv")) {
-                    CSVReader csv = new CSVReader(new InputStreamReader(zis));
-                    List<String[]> csvStrings = csv.readAll();
-                    Assertions.assertEquals(5, csvStrings.size());
-                    Assertions.assertArrayEquals(
-                            new String[]{"Тестовый CSV-документ"},
-                            csvStrings.get(4)
-                    );
-                    Assertions.assertArrayEquals(
-                            new String[]{"Это документ в формате PDF", " который был создан для тестирования загрузки файлов."},
-                            csvStrings.get(2)
-                    );
+            if (zis.getNextEntry() != null) {
+                ZipEntry zipEntry;
+                while ((zipEntry = zis.getNextEntry()) != null) {
+                    if (zipEntry.getName().endsWith(".csv")) {
+                        CSVReader csv = new CSVReader(new InputStreamReader(zis));
+                        List<String[]> csvStrings = csv.readAll();
+                        Assertions.assertEquals(5, csvStrings.size());
+                        Assertions.assertArrayEquals(
+                                new String[]{"Тестовый CSV-документ"},
+                                csvStrings.get(4)
+                        );
+                        Assertions.assertArrayEquals(
+                                new String[]{"Это документ в формате PDF", " который был создан для тестирования загрузки файлов."},
+                                csvStrings.get(2)
+                        );
+                    }
                 }
-            }
+            } else throw new FileNotFoundException("Архив пустой");
         }
     }
 }

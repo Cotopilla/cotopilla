c6c5d4809d98dc76fd4c19b521832cf000578417
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-05-19 20:59:42 +0300

    add selenoid

--- src/test/java/tests/demoqa/RegistrationRemoteTests.java
+++ src/test/java/tests/demoqa/RegistrationRemoteTests.java
@@ -1,12 +1,32 @@
 package tests.demoqa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-public class RegistrationWithPageObjectsTests extends TestBase {
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+@Tag("demoqa")
+public class RegistrationRemoteTests {
 
     protected RegistrationPage registrationPage = new RegistrationPage();
     protected TestData testData = new TestData();
+
+    @BeforeAll
+    static void setBrowserParams() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void closeDriver() {
+        closeWebDriver();
+    }
 
     @Test
     void successfulAllFieldsRegistrationTest() {

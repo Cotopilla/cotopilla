b1d818f8b37d34d872a9a87b3c8504f784a28c5f
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-05-19 21:39:05 +0300

    add attach

--- src/test/java/tests/demoqa/RegistrationRemoteTests.java
+++ src/test/java/tests/demoqa/RegistrationRemoteTests.java
@@ -1,6 +1,9 @@
 package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
@@ -21,10 +24,16 @@ public class RegistrationRemoteTests {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
-    void closeDriver() {
+    void getAttachAndCloseDriver() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
 

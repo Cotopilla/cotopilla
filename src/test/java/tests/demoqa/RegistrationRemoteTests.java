042f0b572d0efb3d7945261bd3b4bc7d4127cb20
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-05-26 13:20:31 +0300

    add setListener

--- src/test/java/tests/demoqa/RegistrationRemoteTests.java
+++ src/test/java/tests/demoqa/RegistrationRemoteTests.java
@@ -4,10 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
@@ -35,6 +32,10 @@ public class RegistrationRemoteTests {
         ));
         Configuration.browserCapabilities = capabilities;
 
+    }
+
+    @BeforeEach
+    void setListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
